3c779c199554385eda55941debf90b10a157a215
Fábio Santos <fabiosantos@ifsul.edu.br>
2026-06-17 08:48:02 -0300

    Exemplo de endereços de usuários

--- api/source/Controller/Addresses.php
+++ api/source/Controller/Addresses.php
@@ -10,22 +10,31 @@ class Addresses extends Api
     public function register (array $data): void
     {
         if(!$this->authToken(2)){
-            $this->call(401, "error", "Acesso negado", "error")->back();
+            $this->call(401, "unauthorized", "Acesso negado", "error")->back();
             return;
         }
         // persistência de dados
         $address = new Address(null, $this->userAuthId, $data["street"], $data["number"]);
         if(!$address->insert()){
-            $this->call(500, "error", "Alguma coisa aconteceu - {$address->getErrorMessage()}", "error")->back();
+            $this->call(500, "internal_server_error", "Alguma coisa aconteceu - {$address->getErrorMessage()}", "error")->back();
             return;
         }
-        $this->call(200, "success", "Endereço registrado com sucesso", "success")->back();
+        $this->call(201, "created", "Endereço registrado com sucesso", "success")->back();
     }
 
     public function update(array $data): void
     {
-        var_dump($data);
-        echo "oi...";
+        if(!$this->authToken(2)){
+            $this->call(401, "unauthorized", "Acesso negado", "error")->back();
+            return;
+        }
+
+        $address = new Address(null, $this->userAuthId, $data["street"], $data["number"]);
+        if(!$address->updateById($data["id"])){
+            $this->call(500, "internal_server_error", "Alguma coisa aconteceu - {$address->getErrorMessage()}", "error")->back();
+            return;
+        }
+        $this->call(200, "success", "Endereço alterado com sucesso", "success")->back();
     }
 
 }
